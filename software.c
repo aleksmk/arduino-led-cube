33b280aa85e066de6f51b76fb5478a5f7610bdcb
Aleks <aleksandar.lazarov1@gmail.com>
2011-11-15 00:30:29 +0100

    Made the add_xy_data funcion and cleaned up the code + saved a few bytes :)

--- software.c
+++ software.c
@@ -3,32 +3,30 @@
 #include <util/delay.h>
 #include <avr/io.h>
 
-void setup() {
-    DDRC = 0x3F;  // HCF Demux output(3) + OE + RST + CLK
-    DDRD = 0xFC;  // Data bus
-    DDRB = 0x03;  // Data bus
 
-    PORTC = ~0x3F;
-    PORTC |= _BV(PC5); // RST on
-    PORTC |= _BV(PC3); // OE on
+void setup() {
+    DDRC = 0x3F;        // 0b00111111 HCF Demux output(3) + OE + RST + CLK
+    DDRD = 0xFC;        // 0b11111100 Data bus
+    DDRB = 0x03;        // 0b00000011 Data bus
+
+    PORTC = ~0x3F;      // PORTC = 0b00000000;
+    PORTC |= _BV(PC3);  // OE on - inverting
+
+    reset_cd();
 }
 
 void toogle_clk(void) {
     PORTC |= _BV(PC4);
-    //_delay_ms(5);
+    //_delay_ms(5);     - it works without this, the HC's can handle the speed
     PORTC &= ~_BV(PC4);
 }
 
-void reset_cd(void) {
+void reset_cd(void) { // non-inverting
     PORTC |= _BV(PC5);
-    //_delay_ms(5);
+    //_delay_ms(5);     - it works without this, the HC's can handle the speed
     PORTC &= ~_BV(PC5);
 }
 
-void go_start(void) {
-    reset_cd();
-    toogle_clk();
-}
 
 void add_data(uint8_t data, uint8_t addr) {
     PORTC = addr;
@@ -56,9 +54,17 @@ void clean() {
     }
 }
 
+// Adds data floor by floor, k is the floor identifier (0 - 7)
+
+void add_xy_data(uint8_t *data, uint8_t k) {
+    for (int i=(k << 3), j=0; j<8; i++, j++) {
+        add_data(data[i], i);
+    }
+}
+
 void add_test_data() {
     clean();
-    oe(1);
+    oe(1); // inverting
     //add_data(0b00001111, 0x00);
     //add_data(0b00001001, 0x01);
     //add_data(0b00001001, 0x02);
@@ -74,16 +80,8 @@ void add_test_data() {
     oe(0);
 }
 
-void add_xy_data(uint8_t *data) {
-    for (int i=0; i<8; i++) {
-        add_data((uint8_t)&data[i], i);
-    }
-}
-
-
 int main() {
     setup();
-    PORTC &= ~_BV(PC5); // RST off
 
 
     uint8_t cube[64];
@@ -92,24 +90,21 @@ int main() {
     cube[1] = 0b00001001;
     cube[2] = 0b00001001;
     cube[3] = 0b00001111;
+
     for(int i=4; i<64; i++) {
         cube[i] = 0;
     }
 
 
     while(1) {
-        add_test_data();
-        go_start();
-        _delay_ms(3);
+        reset_cd();
         clean();
-        for(int i=0; i<7; i++) {
-        //    if (i == 6) {
-                add_test_data();
-          //  }
+        for(int i=0; i<8; i++) {
+            add_test_data();
             toogle_clk();
             _delay_ms(3);
         }
-        toogle_clk();
+        toogle_clk();       // so that there isn't any *dead data* on the 7-th floor
     }
 
 
